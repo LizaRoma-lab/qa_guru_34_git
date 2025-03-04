0c090b994ef25b6ceaaf8b0c52a3756f117dd2a6
LizaRoma-lab <73472835+LizaRoma-lab@users.noreply.github.com>
2025-03-04 20:42:49 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,7 @@
 package guru.qa;
 
 public class Main {
-    private static String msg="Hello world!!!!!!!!";
+    private static String msg="Hello world!";
     public static void main(String[] args) {
 
         System.out.println(msg);
